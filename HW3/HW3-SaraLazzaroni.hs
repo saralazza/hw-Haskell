966f03874621dc978c5b289223c84a5414126e3d
saralazza <slazzds@gmail.com>
2024-04-23 16:16:09 +0200

    implement luckyNumbers, I hope :)

--- HW3/HW3-SaraLazzaroni.hs
+++ HW3/HW3-SaraLazzaroni.hs
@@ -5,11 +5,14 @@ insonnia = concat( map (\n -> show n ++ " sheep ") [1..])
 tartaglia = iterate (\row -> zipWith (+) (0 : row) (row ++ [0])) [1]
 
 -- Esercizio 3: Scrivere una funzione Haskell che genera lo stream dei numeri fortunati.
+luckyAux xs = 1 : filterL 3 xs where
+    filterL i (fs:xs) = fs : filterL (i + 1) ([x | (n, x) <- zip [i..] xs, rem n fs /= 0])
 
+luckyNumbers = luckyAux (filter odd [3..])
 
 main :: IO ()
 main = do
-    let ris = take 20 numeriFortunati
+    let ris = take 20 luckyNumbers
     print ris
 
 {-
