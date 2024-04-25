b851a6f8464eae9f998c0cb3fb53b8549b8a4e87
saralazza <slazzds@gmail.com>
2024-04-25 12:06:12 +0200

    finish exercise 4D

--- HW3/HW3-SaraLazzaroni.hs
+++ HW3/HW3-SaraLazzaroni.hs
@@ -12,6 +12,34 @@ luckyAux xs = 1 : filterL 3 xs where
     filterL i (fs:xs) = fs : filterL (i + 1) ([x | (n, x) <- zip [i..] xs, rem n fs /= 0])
 
 luckyNumbers = luckyAux (filter odd [3..])
+
+-- Esercizio 4D.1: scrivere un’equazione ricorsiva che genera l’albero di Calkin-Wilf
+data BinTree a = Node a (BinTree a) (BinTree a) | Empty
+    deriving (Show)
+
+calkinTreeAux (m,n) isLeft = Node newRoot (calkinTreeAux newRoot True) (calkinTreeAux newRoot False)
+    where
+        newRoot = if isLeft then (m, m+n) else (m+n, n)
+
+calkinTree = Node (1,1) (calkinTreeAux (1,1) True) (calkinTreeAux (1,1) False)
+
+-- Esercizio 4D.2: scrivere la funzione takeNlevels::Int -> BinTree a -> BinTree a che taglia un albero (eventualmente infinito) ai primi n livelli; stipulando che takeNlevels 0 b torni sempre l’albero vuoto Empty e takeNlevels 1 (Node r lft rgt) torni Node r Empty Empty, etc.,
+takeNlevels 0 _ = Empty
+takeNlevels 1 (Node x sx dx) = Node x Empty Empty
+takeNlevels n (Node x sx dx) = Node x (takeNlevels (n-1) sx) (takeNlevels (n-1) dx)
+
+-- Esercizio 4D.3: scrivere una funzione visitaLivelli :: BinTree a -> [a] che produce la lista dei valori contenuti nei nodi di un albero (finito) nella sequenza ottenuta da una visita per livelli.
+visitaAux [] = []
+visitaAux (Empty : xs) = visitaAux xs
+visitaAux ((Node val sx dx) : xs) = val : visitaAux (xs ++ [sx, dx]) 
+
+visitaLivelli tree = visitaAux [tree]
+
+main :: IO ()
+main = do
+    let ris = visitaLivelli (takeNlevels 4 calkinTree)
+    print ris
+
 {-
 main :: IO ()
 main = do
@@ -25,7 +53,7 @@ main = do
     print ris
 -}
 
-
+{-
 main :: IO ()
 main = do
     let ris =  tartaglia !! 0
@@ -46,4 +74,4 @@ main = do
     print ris
     let ris =  tartaglia !! 4 !! 2
     print ris
-
+-}
