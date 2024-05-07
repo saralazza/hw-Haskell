baa088d8001c0c23a3faf4ed7dd9dc9ef0e30f8b
saralazza <slazzds@gmail.com>
2024-05-07 10:30:11 +0200

    add comments

--- HW3/HW3-SaraLazzaroni.hs
+++ HW3/HW3-SaraLazzaroni.hs
@@ -10,9 +10,13 @@ tartAux xs = zipWith (+) (0:xs) (xs++[0])
 tartaglia = [1]: map tartAux tartaglia
 
 -- Esercizio 3: Scrivere una funzione Haskell che genera lo stream dei numeri fortunati.
+-- Dalla lista dei numeri che rimangono, il primo numero è il nuovo lucky number e poi filtro sulla lista
+-- rimanente andando a zippare gli elementi della lista con gli indici a partire da i e prendendo solo i
+-- numeri che sono in una posizione non multiplo del nuovo lucky number. In particolare l'indice i viene usato
+-- per tenere conto del numero di lucky number che sono stati già messi nella lista di output.
 luckyAux xs = 1 : filterL 3 xs where
     filterL i (fs:xs) = fs : filterL (i + 1) ([x | (n, x) <- zip [i..] xs, rem n fs /= 0])
-
+-- Elimino tutti i numeri pari per il 2
 luckyNumbers = luckyAux (filter odd [3..])
 
 -- Esercizio 1D.1: Scrivere in Haskell il funzionale primRec che definisce la ricorsione primitiva
@@ -29,8 +33,6 @@ primRec' h g n = snd ((for (\(x, y) -> (x + 1, h x y)) n) (0,g))
 -- Soluzione: E' possibile definirla facilmente con i numerali di Church in quanto essi simulano lo stesso comportamento del for andando a comporre la funzione tante volte
 
 -- Esercizio 2D.1: Scrivere un one-liner Haskell partsFromAll tale che partsFromAll n allPartitions sia proprio la lista di liste che rappresenta le partizioni di n (in ordine ascendente, preferibilmente).
-
--- TODO: da modificare
 partsFromAll n xss = (takeWhile (\xs -> n /= head xs) (map (\xs -> take (length (takeWhile (< n) (scanl (+) 0 xs))) xs) xss)) ++ [[n]]
 
 -- Esercizio 2D.2: Scrivere un’equazione ricorsiva che genera allPartitions.
@@ -43,13 +45,24 @@ discendenti (x:y:xs)
     | x<y = False
     | otherwise = discendenti (y:xs)
 
+-- Idea: se sono nel caso n, allora prendo tutte le partizioni già generate che hanno somma (n-1), da queste
+-- ottengo la nuova partizione andando ad aggiugnere 1 all'ultimo valore di ogni lista con la funzione 
+-- newPartition ma delle partizioin risultanti prendo solo quelle discendenti. Dopo aver calcolato tutte le 
+-- nuove partizioni per n, aggiungo ad ognuna la lista infinita di 1 e poi faccio la chiamata ricorsiva per (n+1)
 allPartitions = (repeat 1) : allPartitionsAux 2 where
     allPartitionsAux n = map ( ++ repeat 1) (filter discendenti (map newPartition (partsFromAll (n-1) allPartitions))) ++ allPartitionsAux (n+1)
     
--- Esercizio 2D.3: Sviluppare qualche idea per rappresentare altre strutture combinatorie in modo analogo, tipo: tutti i sottoinsiemi (finiti) dei Naturali.
+-- Esercizio 2D.3: Sviluppare qualche idea per rappresentare altre strutture combinatorie in modo analogo, tipo: tutti i sottoinsiemi (finiti) dei Naturali e tutte le permutazioni (a dominio finito) dei Naturali.
+-- Idea: se sono nel caso n, prendo tutti i sottoinsiemi già generati che iniziano con un valore minore di n
+-- (devo farlo perchè allSubSet è uno stream infinito e devo usare takeWhile), a questi sottoinsiemi aggiungo
+-- n in testa e poi faccio la chiamata ricorsiva per (n+1)
 allSubSet = [] : (allSubSetAux 1) where
     allSubSetAux n = (map (n:) (takeWhile (\xs ->  (xs == []) || (n > head xs)) allSubSet)) ++ (allSubSetAux (n+1))
 
+-- Idea: e sono nel caso n, prendo tutte le permutazioni già generate che iniziano con un valore minore di n
+-- (devo farlo perchè allPermutations è uno stream infinito e devo usare takeWhile) e da ognuna di queste genero
+-- tutte le nuove permutazioni possibili. Quindi data una permutazione già esistente, inserisco n in tutte le possibili
+-- posizioni nella permutazione. Alla fine faccio la chiamata ricorsiva per (n+1)
 nextPermutation x i ys =  y1 ++ [x] ++ y2 where
     (y1, y2) = splitAt i ys
 
@@ -60,6 +73,15 @@ allPermutations = [] : allPermutationsAux 1 where
     allPermutationsAux n = (concatMap (\xs -> nextPermutations n 0 xs) ((takeWhile (\xs ->  (xs == []) || (n > head xs)) allPermutations))) ++ (allPermutationsAux (n+1))
 
 -- Esercizio 3D.1: Date una definizione circolare dei numeri di Ulam, usando allSums ulams
+-- Idea: 
+--      - ndiag rappresenta il numero di diagonali che devo andare a leggere per trovare il nuovo numero di ulams
+--      - x è l'ultimo numero di ulam che ho preso
+--      Per calcolare il nuovo numero di ulams calcolo tutte le possibili somme (con la funzione allSums) 
+--      generate dai numeri di ulams già calcolati presenti nella lista ulams. Dalla matrice di tutte
+--      le somme possibili prendo le prime ndiag diagonali (mi serve perchè ho uno stream infinito di
+--      stream infinit) e le unisco in una lista. Da questa lista prendo solo i valori che compaiono una volta 
+--      perchè significa che sono fatti in modo unico con la somma di due numeri di ulam precedentemente 
+--      calcolati. Dalla lista filtrata prendo il minimo che sarà il nuovo numero di ulams.
 allSums [] = []
 allSums (x:xs) = map (x+) xs:allSums xs
 
@@ -81,7 +103,9 @@ ulams = 1:2: ps where
 -- Esercizio 4D.1: scrivere un’equazione ricorsiva che genera l’albero di Calkin-Wilf
 data BinTree a = Node a (BinTree a) (BinTree a) | Empty
     deriving (Show)
-
+-- I valori dei nodi dell'albero sono una coppia che rappresenta (m,n). Se mi trovo in un figlio sinistro
+-- di qualche padre (isLeft = True), allora il valore della radice di questo albero sarà (m,m+n), altrimenti
+-- (isLeft = False) sarà (m+n,m)
 calkinTreeAux (m,n) isLeft = Node newRoot (calkinTreeAux newRoot True) (calkinTreeAux newRoot False)
     where
         newRoot = if isLeft then (m, m+n) else (m+n, n)
@@ -94,6 +118,7 @@ takeNlevels 1 (Node x sx dx) = Node x Empty Empty
 takeNlevels n (Node x sx dx) = Node x (takeNlevels (n-1) sx) (takeNlevels (n-1) dx)
 
 -- Esercizio 4D.3: scrivere una funzione visitaLivelli :: BinTree a -> [a] che produce la lista dei valori contenuti nei nodi di un albero (finito) nella sequenza ottenuta da una visita per livelli.
+-- Idea: aggiungo i nodi di cui devo visitare i figli in una lista.
 visitaAux [] = []
 visitaAux (Empty : xs) = visitaAux xs
 visitaAux ((Node val sx dx) : xs) = val : visitaAux (xs ++ [sx, dx]) 
@@ -102,63 +127,5 @@ visitaLivelli tree = visitaAux [tree]
 
 main :: IO ()
 main = do
-    let ris = take 65 allPermutations
+    let ris = "HW-SaraLazzaroni (1983548)"
     print ris
-
-{-main :: IO ()
-main = do
-    let ris = take 32 allSubSet 
-    print ris-}
-
-{-main :: IO ()
-main = do
-    let ris = partsFromAll 8 allPartitions
-    print ris-}
-
-{-main :: IO ()
-main = do
-    let ris = take 100 ulams
-    print ris-}
-
-{-
-main :: IO ()
-main = do
-    let ris = visitaLivelli (takeNlevels 4 calkinTree)
-    print ris
--}
-
-{-
-main :: IO ()
-main = do
-    let ris = take 57 luckyNumbers
-    print ris-}
-
-{-
-main :: IO ()
-main = do
-    let ris = take 1000 insonnia
-    print ris
--}
-
-{-
-main :: IO ()
-main = do
-    let ris =  tartaglia !! 0
-    print ris
-    let ris =  tartaglia !! 1
-    print ris
-    let ris =  tartaglia !! 2
-    print ris
-    let ris =  tartaglia !! 3
-    print ris
-    let ris =  tartaglia !! 4
-    print ris
-    let ris =  tartaglia !! 5
-    print ris
-    let ris =  tartaglia !! 6
-    print ris
-    let ris =  tartaglia !! 7
-    print ris
-    let ris =  tartaglia !! 4 !! 2
-    print ris
--}
